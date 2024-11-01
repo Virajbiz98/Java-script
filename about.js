37ac730a888c1e7d98fafc8ad0b4b80b90b01e54
Virajbiz98 <Virajbiz98@gmail.com>
2024-11-02 00:55:34 +0530

    Create about.js

---
+++ about.js
@@ -0,0 +1,31 @@
+// 1. What is JavaScript? 
+//JavaScript (JS) is a programming language for web development.
+//Runs in the browser to make web pages interactive. 
+// Works with HTML and CSS. 
+
+// 2. How to Write JavaScript 
+// Place JavaScript code inside <script> tags in an HTML file. 
+// Example: 
+html 
+ <script>
+  console.log("Hello, World!"); 
+ </script> 
+//3. Variables
+//used to stor data
+//use let,const, or var to declare
+//example
+let name = "john";
+const age = 26;
+
+// Data Types
+//string - Text- e.g., "hello"
+//number - e.g., 2,1,18,12,
+//Array - list of items e.g., [1,2,3,4,5]
+//Object - key - value pairs e.g.,{name:"john", age: 25}
+
+// Basic operators
+// math: +, - , * /
+// Comparison: ==, ===, !=, <,>
+// Lgical: &&(AND), ||(OR) !(NOT)
+
+
