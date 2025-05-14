6a6051fc11281a1c3cfcc5aa8b8f9768585be0b2
virajbiz98 <virajbiz98@gmail.com>
2025-05-14 12:53:02 +0500

    add dis file

--- bolt_cv/vite.config.ts
+++ bolt_cv/vite.config.ts
@@ -4,17 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://extensions.aitopia.ai',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        secure: false,
-      }
-    }
-  },
+  base: 'Java-script'
 });
